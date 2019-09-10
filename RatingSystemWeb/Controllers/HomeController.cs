c6379bf7a5a0b9bea48a5316cffd34670c2e0bab
lchoros <choros777@gmail.com>
2019-09-10 13:54:15 +0100

    add rating web system

---
+++ RatingSystemWeb/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RatingSystemWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DrawRating(int rating)
+        {
+
+            var fullStars = rating / 10;
+            var emptyStars = (100 - rating) / 10;
+            var halfStars = 10 - fullStars - emptyStars;
+            var stars = "";
+            for (int i = 0; i < fullStars; i++)
+            {
+                stars += "<img src='/Content/icons/starfull.png' />";
+            }
+            if(halfStars > 0)
+            {
+                stars += "<img src='/Content/icons/starhalf.png' />";
+            }
+            for (int i = 0; i < emptyStars; i++)
+            {
+                stars += "<img src='/Content/icons/starempty.png' />";
+            }
+            ViewBag.Rating = rating;
+            ViewBag.Stars = stars;
+            return View("Index", "", stars);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Stop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Stop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int width = 4 * n + 3;
+            int dots = n + 1;
+            int underscore = 2 * n + 1;
+            Console.WriteLine("{0}{1}{0}", new string('.', dots), new string('_', underscore));
+            underscore -= 2;
+            dots--;
+            for (int row = 0; row < n; row++)
+            {
+                Console.WriteLine("{0}//{1}\\\\{0}", new string('.', dots), new string('_', underscore));
+                dots--;
+                underscore += 2;
+            }
+            Console.WriteLine("//{0}STOP!{0}\\\\", new string('_', (underscore - 5) / 2));
+            for (int row = 0; row < n; row++)
+            {
+                Console.WriteLine("{0}\\\\{1}//{0}", new string('.', row), new string('_', underscore));
+                underscore -= 2;
+            }
+            Console.WriteLine();
+        }
+    }
+}
